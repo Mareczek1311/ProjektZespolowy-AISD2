f6f4e658f1117a58f8d2a5f8677b69a17b20f850
Mareczek1311 <mkretkowski131@gmail.com>
2024-04-16 11:31:30 +0200

    implementacja merge sort zamiast bubble sort oraz dodanie przykladowych danych z lekcji

--- zadanie1/graham.py
+++ zadanie1/graham.py
@@ -1,8 +1,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-
+from functools import cmp_to_key
 
 ###powinny byc z pliku ladowane i powinien robic to inny program !DO POPRAWIENIA!
+"""
 dane = [
     (1, 2),
     (3, 4),
@@ -16,12 +17,26 @@ dane = [
     (5, 5),
     (6, 6),
 ]
+"""
+
+dane = [
+    (-4, -5),
+    (-3, -2),
+    (5,3),
+    (7,5),
+    (4,-1),
+    (2,-3),
+    (6,5),
+    (-5,-4),
+    (5,1)
+]
 
 class Gragam:
     def __init__(self, punkty) -> None:
         self.calculated = False
         self.punkty = punkty
         self.convexHull = []
+        self.min = None
 
     def findMin(self, punkty):
         min = punkty[0]
@@ -30,7 +45,7 @@ class Gragam:
             if punkty[i][1] < min[1]:
                 min = punkty[i]
             elif punkty[i][1] == min[1]:
-                if punkty[i][0] < min[0]:
+                if punkty[i][0] > min[0]:
                     min = punkty[i]
         return min
 
@@ -38,22 +53,33 @@ class Gragam:
     def det(self, p1, p2, p3):
         return p1[0]*p2[1] + p2[0]*p3[1] + p3[0]*p1[1] - p3[0]*p2[1] - p2[0]*p1[1] - p1[0]*p3[1]
 
+    def distSq(self, p1, p2):
+        return ((p1[0] - p2[0]) * (p1[0] - p2[0]) +
+                (p1[1] - p2[1]) * (p1[1] - p2[1]))
+
+    def compare(self, p1, p2):
+        wyz = self.det(self.min, p1, p2)
+        
+        if wyz == 0:
+            if self.distSq(self.min, p2) >= self.distSq(self.min, p1):
+                return -1
+            else:
+                return 1
+        else:
+            if wyz > 0:
+                return -1
+            else:
+                return 1
+
     def algorytm(self):
         punkty = self.punkty
-        min = self.findMin(punkty)
 
-        ###bubble sort (n^2) zla zlozonosc !DO POPRAWIENIA!
-        for i in range(1, len(punkty)):
-            for j in range(i+1, len(punkty)):
-                wyz = self.det(min, punkty[i], punkty[j])
-                if wyz < 0:
-                    punkty[i], punkty[j] = punkty[j], punkty[i]
-                if wyz == 0:
-                    if punkty[i][0] < punkty[j][0]:
-                        punkty[i], punkty[j] = punkty[j], punkty[i]
+        self.min = self.findMin(punkty)
+        
+        punkty = sorted(punkty, key=cmp_to_key(self.compare))
 
         stack = []
-        stack.append(min)
+        stack.append(punkty[0])
         stack.append(punkty[1])
         stack.append(punkty[2])
 
@@ -62,7 +88,7 @@ class Gragam:
             while self.det(stack[-2], stack[-1], punkty[i]) < 0:
                 stack.pop()
             stack.append(punkty[i])
-        stack.append(min)
+        stack.append(punkty[0])
         
         self.convexHull = stack
     
