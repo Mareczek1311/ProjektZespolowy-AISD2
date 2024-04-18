93609c3bf101a50d749ee4d10f0dbb479db71801
Mareczek1311 <mkretkowski131@gmail.com>
2024-04-18 20:06:58 +0200

    Main juz obsluguje algorytm fordaFulkersona

--- zadanie1/src/main.py
+++ zadanie1/src/main.py
@@ -13,13 +13,27 @@ class Main:
 
     def run(self):
         self.data = self.fr.read()
+
+        #Gragam
         self.punkty = self.getPunkty()
         graham = Graham(self.punkty)
+        print("Otoczka: ")
         print(graham.getOtoczka())
         graham.draw()
 
+
+        #FordFulkerson
+        n = len(self.punkty)
+        solver = FordFulkerson(n, self.data["start"], self.data["end"], self.punkty)
+        solver.config(self.data["start"], self.data["end"], self.data["adjList"])
+        print(solver.getMaxFlow())
+        print("Graph:")
+        solver.getGraph()
+        solver.draw()
+        
+
 def main():
-    m = Main('../data/przyklad2.txt')
+    m = Main('../data/przyklad3.txt')
     m.run()
 
 if __name__ == '__main__':
