00ff9a494abcfd56548bbdfebf3d725f6c062c93
Jakub Dołasiński <127053976+JakubDolas@users.noreply.github.com>
2024-04-19 22:04:41 +0200

    zaczete generowanie trasy :3

--- Zadanie3/zad.py
+++ Zadanie3/zad.py
@@ -4,10 +4,28 @@ class Plaszczak:
     def __init__(self, numer, energia):
         self.numer = numer
         self.energia = energia
-        self.trasa = []
+        self.trasa = [] 
 
     def wygeneruj_plot(self, liczba_punktow, jasnosci):
         self.trasa = [random.choice(jasnosci) for _ in range(liczba_punktow)]
+
+    # punkt_startowy - od ktorego zaczyna sie straznik
+    # punkt_zatrzymania - miejsca w ktorych musi sie zatrzymac podczas trasy
+    # aktualny_punkt - obecne miejsce tego barana
+
+
+    #dokonczyc
+    def wygeneruj_trase_straznika(punkt_orientacyjny, straznik, maks_punkt_zatrzymania):
+        trasa = []
+        punkt_startowy = punkt_orientacyjny[0]
+        aktualny_punkt = punkt_startowy
+        punkty_zatrzymania = 0
+
+        while True:
+            trasa.append(aktualny_punkt)
+            punkty_zatrzymania += 1
+
+
 
 plaszczak = Plaszczak(1, 10)
 jasnosci = [1, 2, 3, 4, 5]
