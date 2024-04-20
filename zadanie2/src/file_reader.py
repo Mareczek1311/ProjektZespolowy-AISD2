0389baa4dbeac54c449c363fdade5d25155277af
Kacper <kacperkk129@gmail.com>
2024-04-20 19:27:09 +0200

    filereader dla elegancji (chce byc super jak Marek)

---
+++ zadanie2/src/file_reader.py
@@ -0,0 +1,29 @@
+class File_reader:
+    def __init__(self, filename):
+        self.filename = filename
+        self.lines = 0
+        self.words = 0
+        self.wordsToReplace = {}
+        self.text = ""
+
+    def readText(self):
+        with open(self.filename, 'r') as file:
+            data = file.readline().strip()
+            n, m = data.split()
+            self.lines = int(n)
+            self.words = int(m) 
+            
+            for i in range(self.lines):
+                line = file.readline()
+                self.text += line
+
+            for i in range(self.words):
+                data = file.readline().strip()
+                wrongWord, correctWord = data.split()
+                self.wordsToReplace[wrongWord] = []
+                self.wordsToReplace[wrongWord].append(correctWord)
+
+#przyklad uzycia
+
+# f = File_reader('../tests/test1.txt')
+# f.readText()
