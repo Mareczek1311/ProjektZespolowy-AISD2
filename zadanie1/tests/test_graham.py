24defd389db1f8433fde6cb450117d39fd08f19f
Marek Kretkowski <mkretkowski131@mat.umk.pl>
2024-05-09 14:43:49 +0200

    testy do funkcju

--- zadanie1/tests/test_graham.py
+++ zadanie1/tests/test_graham.py
@@ -4,32 +4,26 @@ sys.path.append('../src')
 from graham import Graham
 
 class TestGraham:
-    def test_det_1(self):
+    @pytest.mark.parametrize("points, expected", [
+        ([(1, 1), (2, 3), (4, 5)], -2),
+        ([(4, 5), (2, 3), (1, 1)], 2),
+        ([(1, 2), (3, 6), (5, 10)], 0),
+        ([(0, 0), (0, 0), (0, 0)], 0),
+        ([(-1, -2), (-3, -4), (-5, -6)], 0),
+        ([(2, 1), (4, 2), (6, 3)], 0),
+        ([(2, 1), (4, 2), (6, 3)], 0)])
+    def test_det(self, points, expected):
         g = Graham()
-        assert g.det((1, 1), (2, 3), (4, 5)) == -2
-
-    def test_det_2(self):
-        g = Graham()
-        assert g.det((4, 5), (2, 3), (1, 1)) == 2
-
-    def test_det_3(self):
-        g = Graham()
-        assert g.det((1, 2), (3, 6), (5, 10)) == 0
-
-    def test_det_4(self):
-        g = Graham()
-        assert g.det((0, 0), (0, 0), (0, 0)) == 0
-
-    def test_det_5(self):
-        g = Graham()
-        assert g.det((-1, -2), (-3, -4), (-5, -6)) == 0
+        assert g.det(points[0], points[1], points[2]) == expected
     
-    def test_det_6(self):
-        g = Graham()
-        assert g.det((1, 2), (2, 4), (3, 6)) == 0
 
-    def test_det_7(self):
+    @pytest.mark.parametrize("points, expected", [
+        ([[0,3], [3,1], [2,2]], [3,1]), #klasyczny przypadek, punkt 3,1 ma najmniejsza wartosc y 
+        ([[0,3], [1,2], [1,1], [2,1]], [2,1]), #przypadek gdy punkty maja taka sama wartosc y
+        ([[0,3], [3,1], [1,2], [1,1], [2,1]], [3,1]),#przypadek gdy punkt 3,1 ma najwieksza wartosc x
+        ([(2, 2)], (2, 2))])  #przypadek gdy jest tylko jeden punkt
+    def test_find_min(self, points, expected):   
         g = Graham()
-        assert g.det((2, 1), (4, 2), (6, 3)) == 0
+        assert g.findMin(points) == expected
 
     
